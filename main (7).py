e3ccc0baf75733185ca9ab9528a9f9bd8b415e1d
camilaalmeidaa <99203530+camilaalmeidaa@users.noreply.github.com>
2022-04-08 16:46:19 -0300

    Add files via upload

---
+++ main (7).py
@@ -0,0 +1,26 @@
+import q1_str
+import q2_str
+import q3_str
+import q4_str
+import q5_str
+import q6_str
+import q7_str
+import q8_str
+import q9_str
+import q10_str
+import q12_str
+import q14_str
+
+q1_str.compare_str("Brasil", "Hexa")
+q2_str.print_name_reverse()
+q3_str.print_name()
+q4_str.print_nome_escada()
+q5_str.print_nome_escada_inverso()
+q6_str.converte_data()
+q7_str.calcular_ocorrencias("Oi meu nome é Camila")
+q8_str.check_palindrome("osso")
+q9_str.checa_cpf()
+q10_str.converter_numero(98)
+q12_str.valida_telefone("465-1916")
+q14_str.leet_translator()
+
